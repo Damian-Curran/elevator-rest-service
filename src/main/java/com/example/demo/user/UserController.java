01a1e17761463945ac60eeb4ccc247a9a8d7ce8e
Damian-Curran <sgt.curren@gmail.com>
2021-05-11 02:04:46 +0100

    Cleaning

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -1,10 +1,8 @@
 package com.example.demo.user;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
