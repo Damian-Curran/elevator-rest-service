894b17cdcc3fdc51dd0a7ba53364dcd8e52442be
Damian-Curran <sgt.curren@gmail.com>
2021-05-12 04:01:39 +0100

    Added basic info logging to service classes

--- src/main/java/com/example/demo/elevator/ElevatorService.java
+++ src/main/java/com/example/demo/elevator/ElevatorService.java
@@ -3,31 +3,39 @@ package com.example.demo.elevator;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ElevatorService {
+	Logger logger = LogManager.getLogger(ElevatorService.class);
 	@Autowired 
 	private ElevatorRepository er;
 	
-	public void add(Elevator b){
-		er.save(b);
+	public void add(Elevator el){
+		logger.info("Adding elevator <>", el);
+		er.save(el);
 	}
 	
 	public Elevator getById(Long id){
+		logger.info("Getting elevator <>", id);
 		return er.findOneById(id);
 	}
 	
 	public void delete(long id){
+		logger.info("Deleting elevator <>", id);
 		er.deleteById(id);
 	}
 	
-	public void update(Elevator u){
-		er.save(u);
+	public void update(Elevator el){
+		logger.info("Updating elevator <>", el);
+		er.save(el);
 	}
 	
 	public List<Elevator> getAll(){
+		logger.info("Getting all elevators");
 		List<Elevator> allElevators = new ArrayList<Elevator>();
 		er.findAll().forEach(allElevators:: add);
 		
--- src/main/java/com/example/demo/user/UserService.java
+++ src/main/java/com/example/demo/user/UserService.java
@@ -3,31 +3,39 @@ package com.example.demo.user;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+	Logger logger = LogManager.getLogger(UserService.class);
 	@Autowired 
 	private UserRepository ur;
 	
 	public void add(User u){
+		logger.info("Adding user <>", u);
 		ur.save(u);
 	}
 	
 	public User getById(Long id){
+		logger.info("Getting user <>", id);
 		return ur.findOneById(id);
 	}
 	
 	public void delete(long id){
+		logger.info("Deleting user <>", id);
 		ur.deleteById(id);
 	}
 	
 	public void update(User u){
+		logger.info("Updating user <>", u);
 		ur.save(u);
 	}
 	
 	public List<User> getAll(){
+		logger.info("Getting all users");
 		List<User> allUsers = new ArrayList<User>();
 		ur.findAll().forEach(allUsers:: add);
 		
